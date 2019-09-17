0ba20685b6bae5fda01fe00058a17f3236253162
Michael Waldo <mbwcogic@yahoo.com>
2019-09-17 20:47:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,11 +8,13 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.reverse
 end
 
 def using_first(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.first
 end
 
 def using_last(array)
